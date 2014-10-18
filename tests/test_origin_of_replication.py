0ef4f9e2b3bbbece9baf8bc4e2f92df2e49c604e
Brian Sipple <Bsipple57@gmail.com>
2014-10-18 18:20:37 -0500

    Begin adding tests.

---
+++ tests/test_origin_of_replication.py
@@ -0,0 +1,27 @@
+import unittest
+from origin_of_replication.pattern_count import hammingDistance
+
+class OriginOfReplicationTest(unittest.TestCase):
+
+	def setUp(self):
+		super(OriginOfReplicationTest, self).setUp()
+
+	def tearDown(self):
+		super(OriginOfReplicationTest, self).tearDown()
+
+	
+	def test_hamming_distance(self):
+
+		p = "AAACCCTTTGGG"
+		q = "AAACCCTTTGGC"
+
+		hd = hammingDistance(p, q)
+
+		self.assertEquals(1, hd)
+
+
+
+if __name__ == "__main__":
+
+	unittest.main()
+
