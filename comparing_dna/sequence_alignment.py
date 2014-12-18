9715c420202d2bc9eda3968370e3b51e177117ef
Brian Sipple <Bsipple57@gmail.com>
2014-12-18 04:07:05 -0600

    setup for sequence alignment

---
+++ comparing_dna/sequence_alignment.py
@@ -0,0 +1,6 @@
+def longet_common_subsequence(string1, string2):
+    """
+    Finds A longest common subsequence between two strings using
+    insertion (string1) and deletion (string2) methods
+    """
+    pass
--- genome_sequencing/genome_sequencing.py
+++ genome_sequencing/genome_sequencing.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from itertools import product
+from collections import defaultdict
 import numpy as np
 
 
@@ -18,43 +19,21 @@ def sequence_composition(k, sequence):
     return list(np.sort(res))
 
 
-def sequence_reconstruction(kmers, string_result=True):
-    """
-    The inverse of sequence composition... GIVEN a composition of k-mers, we
-    need to reconstruct the k-mer composition back into the sequence string
-    from which they came.
 
-    To do this, we analyze the suffix of the first
-    k-mer (kmner[1:] and look to see if it matches the prefix of the
-    second k-mer (kmer[:-1])
+def string_from_path(kmers):
 
-    If so, it's added to our genome string.
+    result = kmers[0]
 
-    :param kmers: a list of k-mers representing a DNA string's k-mer composition
-    :return: the DNA string that would have generated the composition
-    """
-    k = len(kmers[0])
-    current_string_suffix = kmers[0][0]  # start with the first character of the first k-mer
-
-
-    # for k-mers 1 to n-1, (1 being the second), overwrite the current string at i+1 with the first k-1 characters
-    i = 1
     for kmer in kmers[1:]:
-        next_string_prefix = kmer[0:-1]
-        current_string_suffix = current_string_suffix[:i] + next_string_prefix
-        i+=1
+        result += kmer[-1]
 
-    current_string_suffix += kmers[-1][-1]  # end with the last character of the last k-mer
-
-    result = current_string_suffix  # To be clear: the final suffix is our result
-
-    return "".join([char for char in result])
+    return result
 
 
 
 
 
-def overlap_list(kmers, return_list=True):
+def overlap_graph(kmers):
     """
     Constructs an overlap graph (In the form of an adjacency list)
     for a list of k-mers.
@@ -63,60 +42,92 @@ def overlap_list(kmers, return_list=True):
     each of the other k-mers where the k-1-mer prefix of kmer and k-1k-mer
     suffix of kmer' match.
     """
+    prefix = {}
+    for i in kmers:
+        p = i[:-1]
+        if p in prefix:
+            prefix.get(p).append(i)
+        else:
+            prefix.update({p: [i]})
 
-    # Lambda functions to check for overlapping prefixes and suffixes and print them
-    check_overlap = lambda pair: pair[0][1:] == pair[1][:-1]
-    print_overlap = lambda pair: ' -> '.join(pair)
-
-    # Get all pairs, filter out non-overlapping pairs, then print remaining overlapping pairs
-    pairs = ([kmer1, kmer2] for i, kmer1 in enumerate(kmers) for j, kmer2 in enumerate(kmers) if i != j)
-    overlaps = map(print_overlap, filter(check_overlap, pairs))
-
-    # By if return_list is true, we'll return the list (in lexicographic order) to the caller instead of printing it
-    if return_list:
-        return list(np.sort([overlap for overlap in overlaps]))
-    else:
-        with open('../output_data/genome_seq__overlap_graph.txt', 'w') as output_data:
-            output_data.write("\n".join(overlaps))
+    for i in kmers:
+        s = i[1:]
+        if s in prefix:
+            for k in prefix.get(s):
+                yield '%s -> %s' % (i, k)
 
 
-def universal_string(k, char_choices):
+
+def universal_string(k):
     """
     Contructs a k-univeral binary string_result
     """
-    print(["".join(item) for item in product(char_choices, repeat=k)])
+    kmers = [''.join(x) for x in list(product('01', repeat=k - 1))]
 
-    possibilities = generate_possible_kmers(k, char_choices)
-    return sequence_reconstruction(possibilities)
+    edges = []
+    for i in overlap_graph(kmers):
+        data = i.split(' -> ')
+        for i in data[1].split(','):
+            edges.append((data[0], i))
+
+    small, large = classify_edges(edges)
+    origin = '0' * (k - 1)
+    start = large.get(origin)
+    cycle_edges = [(origin, small.get(origin)), (origin, start)]
+    while 1:
+        if start in large:
+            cycle_edges.append((start, large.get(start)))
+            end = large.get(start)
+            large.pop(start)
+            start = end
+        elif start in small:
+            cycle_edges.append((start, small.get(start)))
+            end = small.get(start)
+            small.pop(start)
+            start = end
+        else:
+            break
+
+    return glue(cycle_edges)
 
 
-def generate_possible_kmers(k, char_choices):
-    """
-    Generates all possible k-mers for a
-    list of characters and some number, k
-    """
-    return ["".join(item) for item in product(char_choices, repeat=k)]
+
+def glue(edges):
+    k = len(edges[0][0])
+    string = [edges[0][0] + edges[0][1][-1]]
+
+    for i, j in edges[1:]:
+        string.append(j[-1])
+
+    return ''.join(string[:-(k + 1)])
+
+
+def classify_edges(edges):
+    small_edges = {}
+    large_edges = {}
+
+    for i, j in edges:
+        if i in small_edges:
+            large_edges.update({i: j})
+        else:
+            small_edges.update({i: j})
+
+    return small_edges, large_edges
 
 
 
-
-def debruijn_graph(k, text):
+def debruijn_graph(k, sequence):
     """
     Constructs a graph of length |text| - k + 1 in the form
     of an adjacency list
     """
+    kmers = set(gen_kmers(k, sequence))
+    return overlap_graph(kmers)
 
-    de_bruijn_dict = OrderedDict()
 
-    for kmer in (text[i:i+k] for i in range(len(text)-k+1)):
-        if kmer[:-1] in de_bruijn_dict:
-            de_bruijn_dict[kmer[:-1]].add(kmer[1:])
-        else:
-            de_bruijn_dict[kmer[:-1]] = {kmer[1:]}
+def gen_kmers(k, sequence):
+    for i in range(len(sequence)):
+        yield sequence[i: i + k]
 
-    debruijn = [' -> '.join([item[0], ','.join(item[1])]) for item in de_bruijn_dict.items()]
-
-    print(debruijn)
-    return debruijn
 
 
