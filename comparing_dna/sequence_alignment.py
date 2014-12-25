9f4baee28b39c3e8606a465cc278cdc3489a4dae
Brian Sipple <Bsipple57@gmail.com>
2014-12-24 23:23:52 -0600

    setup for "south_or_east" longest path solution

--- comparing_dna/sequence_alignment.py
+++ comparing_dna/sequence_alignment.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def longet_common_subsequence(string1, string2):
     """
     Finds A longest common subsequence between two strings using
@@ -31,3 +33,27 @@ def min_number_of_coins(money, coin_choices):
 
 
     return min_num_coins[-1]
+
+
+def south_or_east(i, j):
+    """
+    computes the length of the longest path to node (i, j) in
+    a rectangular grid in the Manhattan Problem,
+    abiding by the observation that the only way to reach node (i, j)
+    is either by moving south (↓) from (i − 1, j) or east (→) from (i, j − 1).
+
+    :return: max(i, j)
+    """
+    if i == 0 and j == 0:
+        return 0
+
+    x = -1 * (j ** j)  # Initialize an impossible minimum for x and y
+    y = -1 * (i ** i)
+
+    if i > 0:
+        x = south_or_east(i - 1, j) + southward_edge_weight(i-1, j)
+
+    if j > 0:
+        y = south_or_east(i, j - 1) + eastward_edge_weight(i, j-1)
+
+    return max(x, y)
